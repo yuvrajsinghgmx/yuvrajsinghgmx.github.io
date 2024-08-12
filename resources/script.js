bb304310186e721f80e1494778374d5e5ca16fd6
yuvrajsinghgmx <qomfortzone@gmail.com>
2024-08-12 17:04:33 +0530

    Update

--- resources/script.js
+++ resources/script.js
@@ -6,7 +6,7 @@ window.onload = function () {
     setTimeout(() => {
       splashscreen.classList.add("hidden");
       Maincontent.classList.remove("hidden");
-    }, 5800);
+    }, 1000);
   }
   timedClassChange();
 
